942439a0fe74b42e036c74c1230ae8807d93e88e
Fabio Lanza <fabio.lanza4@gmail.com>
2023-06-03 18:05:29 -0700

    delete layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
 import "./App.css";
-import Layout from "./components/Layout/Layout";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import TourDetails from "./pages/Tour-Details/TourDetails";
+import Tours from "./pages/Tours/Tours";
+import SearchResultList from "./pages/SearchResultList/SearchResultList";
+import ThankYou from "./pages/Thank-you/ThankYou";
+import About from "./pages/About/About";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Layout />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/tours" element={<Tours />} />
+        <Route path="/tours/:id" element={<TourDetails />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/thank-you" element={<ThankYou />} />
+        <Route path="/tours/search" element={<SearchResultList />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
